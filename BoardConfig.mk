CyanogenMod/android_device_lge_mako

mako: enable kernel user_debug traces
To help debugging b/10920955 - N4 runtime crashed in a phone call (twice)

Change-Id: Ifeb9c26a7ca8228bfa82fcd057058c2878014aed
Signed-off-by: Iliyan Malchev <malchev@google.com>

BoardConfig.mk
@@ -27,7 +27,7 @@ TARGET_NO_BOOTLOADER := true
 
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677 user_debug=31
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
 BOARD_USES_ALSA_AUDIO:= true

rpadula/aosp_android_device_lge_geeb_att_us

BoardConfig.mk
@@ -27,7 +27,7 @@ TARGET_NO_BOOTLOADER := true
 
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttySHL0,115200,n8 androidboot.hardware=geeb_att_us lpj=67677
+BOARD_KERNEL_CMDLINE := console=ttySHL0,115200,n8 androidboot.hardware=geeb_att_us lpj=67677 user_debug=31
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
 BOARD_USES_ALSA_AUDIO:= true